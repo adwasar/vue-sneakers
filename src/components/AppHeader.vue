6354b065f0c6b124e3f91ecab7f655a838c5a86f
Vladislav Bryl <vladislavbryl@Vladislavs-Mac-mini.local>
2025-03-07 16:45:15 +0200

    refactor: update tailwind styles

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,25 +6,25 @@ import ProfileIcon from './icons/ProfileIcon.vue';
 
 <template>
   <header
-    class="flex justify-between gap-x-4 md:gap-x-6 xl:gap-x-8 px-4 md:px-8 xl:px-16 py-4 md:py-6 xl:py-8 border-b border-[#EAEAEA]">
+    class="flex justify-between gap-x-4 md:gap-x-6 xl:gap-x-8 px-4 md:px-8 xl:px-16 py-4 md:py-6 xl:py-8 border-b border-gray-200">
     <div class="flex justify-between items-center gap-x-2 md:gap-x-4">
       <img class="w-8 md:w-10 h-8 md:h-10" src="/logo.png" alt="logo">
       <div class="cursor-pointer">
         <h1 class="text-sm md:text-lg xl:text-xl font-bold">VUE SNEAKERS</h1>
-        <p class="text-[8px] md:text-sm text-[#9D9D9D]">Магазин лучших кроссовок</p>
+        <p class="text-[8px] md:text-sm text-gray-400">Магазин лучших кроссовок</p>
       </div>
     </div>
 
     <ul class="flex gap-x-4 md:gap-x-6 xl:gap-x-8">
-      <li class="flex items-center gap-x-2 cursor-pointer text-[#5C5C5C] font-semibold hover:text-black">
+      <li class="flex items-center gap-x-2 cursor-pointer text-gray-600 font-semibold hover:text-black">
         <CartIcon />
         <span class="hidden xl:inline">600&nbsp;грн</span>
       </li>
-      <li class="flex items-center gap-x-2 cursor-pointer text-[#5C5C5C] hover:text-black">
+      <li class="flex items-center gap-x-2 cursor-pointer text-gray-600 hover:text-black">
         <HeartIcon />
         <span class="hidden xl:inline">Закладки</span>
       </li>
-      <li class="flex items-center gap-x-2 cursor-pointer text-[#5C5C5C] hover:text-black">
+      <li class="flex items-center gap-x-2 cursor-pointer text-gray-600 hover:text-black">
         <ProfileIcon />
         <span class="hidden xl:inline">Профиль</span>
       </li>
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,17 +1,18 @@
 <template>
   <article
-    class="relative flex flex-col gap-2 md:gap-3 min-h-40 w-full px-4 md:px-8 py-2 md:py-4 bg-white text-left rounded-[16px] lg:rounded-[24px] border border-[#F3F3F3]">
-    <div class="relative h-28 w-28 md:w-33 overflow-hidden">
+    class="relative flex flex-col gap-2 md:gap-3 w-full px-4 md:px-8 py-2 md:py-4 bg-white text-left rounded-[16px] lg:rounded-[24px] border border-gray-200">
+    <div class="relative self-center h-28 w-28 md:w-33 overflow-hidden">
       <img src="/sneakers/sneakers-1.jpg" alt="Sneakers pair" class="h-auto w-full absolute bottom-0">
     </div>
     <h3 class="text-xs md:text-sm">Мужские Кроссовки Nike Blazer Mid Suede</h3>
-    <div class="flex justify-between cursor-pointer">
+    <div class="flex justify-between">
       <div>
-        <span class="text-xs text-[#BDBDBD] uppercase">цена:</span>
+        <span class="text-xs text-gray-400 uppercase">цена:</span>
         <strong class="block text-sm">129$</strong>
       </div>
-      <img src="/plus.svg" alt="#">
+      <img src="/plus.svg" alt="#" class="cursor-pointer">
     </div>
     <img src="/public/like-1.svg" alt="Like" class="absolute w-8 h-8 cursor-pointer">
   </article>
 </template>
+/÷
