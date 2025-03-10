320fc2813daafad2b9dbb9270d91fae454e13f93
Vladislav Bryl <vladislavbryl@Vladislavs-Mac-mini.local>
2025-03-10 15:52:06 +0200

    feat: fill card`s content

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -4,11 +4,14 @@ import ProductCard from './ProductCard.vue';
 const props = defineProps(["productCards"])
 
 const { productCards } = props
+
+console.log(props)
 </script>
+
 
 
 <template>
   <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-10 gap-2 md:gap-4">
-    <ProductCard v-for="(_, index) in productCards" :key="index" />
+    <ProductCard v-for="(card, index) in productCards" :key="index" :card="card" />
   </div>
 </template>
--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,14 +1,22 @@
+<script setup>
+const props = defineProps(['card'])
+
+const { imgPath, title, price } = props.card
+</script>
+
+
+
 <template>
   <article
     class="relative flex flex-col gap-2 md:gap-3 w-full px-4 md:px-8 py-2 md:py-4 bg-white text-left rounded-2xl lg:rounded-3xl border border-gray-200 cursor-pointer transition hover:-translate-y-2 hover:shadow-xl">
     <div class="relative self-center h-28 w-28 md:w-33 overflow-hidden">
-      <img src="/sneakers/sneakers-1.jpg" alt="Sneakers pair" class="h-auto w-full absolute bottom-0">
+      <img :src="imgPath" alt="Sneakers pair" class="h-auto w-full absolute bottom-0">
     </div>
-    <h3 class="text-xs md:text-sm">Мужские Кроссовки Nike Blazer Mid Suede</h3>
-    <div class="flex justify-between">
-      <div>
+    <h3 class="text-xs md:text-sm">{{ title }}</h3>
+    <div class="flex justify-between mt-auto">
+      <div nt>
         <span class="text-xs text-gray-400 uppercase">цена:</span>
-        <strong class="block text-sm">129$</strong>
+        <strong class="block text-sm">{{ price }}$</strong>
       </div>
       <img src="/plus.svg" alt="Plus">
     </div>
