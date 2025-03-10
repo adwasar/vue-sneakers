5898c01e799eb716751be77202cc5901734ea7a8
Vladislav <104913853+adwasar@users.noreply.github.com>
2025-03-10 16:43:51 +0200

    feat: content for cards
    * feat: fill card`s content
    
    * refactor: props tipization
    
    ---------
    
    Co-authored-by: Vladislav Bryl <vladislavbryl@Vladislavs-Mac-mini.local>

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,14 +1,20 @@
 <script setup>
 import ProductCard from './ProductCard.vue';
 
-const props = defineProps(["productCards"])
+const props = defineProps({
+  productCards: {
+    type: Array,
+    required: true
+  }
+})
 
 const { productCards } = props
 </script>
 
 
+
 <template>
   <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 mt-10 gap-2 md:gap-4">
-    <ProductCard v-for="(_, index) in productCards" :key="index" />
+    <ProductCard v-for="(card, index) in productCards" :key="index" :card="card" />
   </div>
 </template>
